cc4450b647f12607caa72acb71cd86c99aec59a9
jela3105 <jela3105@gmail.com>
2021-04-23 20:37:32 -0500

    Create model user

---
+++ models/user.js
@@ -0,0 +1,37 @@
+// name, email, password, img, role, isActive, createdByGoogle
+
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "The name is mandatory"],
+  },
+  email: {
+    type: String,
+    required: [true, "The email is mandatory"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "The password is mandatory"],
+  },
+  image: {
+    type: String,
+  },
+  role: {
+    type: String,
+    required: true,
+    emun: ["ADMIN_ROLE", "USER_ROLE"],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  createdByGoogle: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = model("Users", UserSchema);
