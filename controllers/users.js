b3e15ab867f3f4b94020c4716cd00ee2935a44cf
jela3105 <jela3105@gmail.com>
2021-04-27 20:26:32 -0500

    Encrypt password

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,31 @@
 const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
 const User = require("../models/user");
 
 const getUsers = (req = request, res = response) => {
   const { q, page = 1, apikey } = req.query;
   res.json({ msg: "get API - cotroller", q, page, apikey });
 };
+
 const putUsers = (req, res) => {
   res.status(400).json({ msg: "put request" });
 };
+
 const postUsers = async (req, res = response) => {
   const { name, email, password, role } = req.body;
   const user = new User({ name, email, password, role });
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
+
   await user.save();
-  res.json({ message: "post API request", user });
+
+  res.json({ user });
 };
+
 const deleteUsers = (req, res) => {
   res.json({ msg: "delete request" });
 };
+
 const patchUsers = (req, res) => {
   res.status(200).json({ msg: "patch request" });
 };
