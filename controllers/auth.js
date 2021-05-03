40b3f074f662dbab780ec1dc7cfca5442f2ad741
jela3105 <jela3105@gmail.com>
2021-05-02 20:34:54 -0500

    Create login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,36 @@
 const { response } = require("express");
-const login = (req, res) => {
-  res.json({
-    msg: "Login ok",
-  });
+const bcryptjs = require("bcryptjs");
+const User = require("../models/user");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "User / Password incorrect - email" });
+    }
+    if (!user.isActive) {
+      return res
+        .status(400)
+        .json({ msg: "User / Password incorrect - not active" });
+    }
+
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res
+        .status(400)
+        .json({ msg: "User / Password incorrect - incorrect" });
+    }
+
+    res.json({
+      msg: "Login ok",
+    });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ msg: "Sorry, something gone wrong at the database" });
+  }
 };
 
 module.exports = {
