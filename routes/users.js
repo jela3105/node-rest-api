ce3f1307b50eb84cdc68b955d71739bb555d1702
jela3105 <jela3105@gmail.com>
2021-05-05 14:09:53 -0500

    Optimize imports

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const { validateFields } = require("../middlewares/validate-fields");
-const { validateJWT } = require("../middlewares/validate-jwt");
-const { isAdmin, hasRole } = require("../middlewares/validate-roles");
+const { validateFields, validateJWT, hasRole } = require("../middlewares");
 
 const {
   isValidRole,
