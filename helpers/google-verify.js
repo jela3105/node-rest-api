e6b11d2cbfb86a44b1528e007f298e5801b7c4a7
jela3105 <jela3105@gmail.com>
2021-05-07 17:00:00 -0500

    Fix typo in idToken

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -2,14 +2,14 @@ const { OAuth2Client } = require("google-auth-library");
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-const googleVerify = async (id_token) => {
+const googleVerify = async (idToken) => {
   const ticket = await client.verifyIdToken({
-    id_token,
+    idToken,
     audience: process.env.GOOGLE_CLIENT_ID,
   });
-  const payload = ticket.getPayload();
+  const { name, picture, email } = ticket.getPayload();
 
-  return payload;
+  return { name, picture, email };
 };
 
 module.exports = { googleVerify };
