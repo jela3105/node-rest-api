88a2634a92cdede77b80aaee705ff2d87d5abdf2
jela3105 <jela3105@gmail.com>
2021-05-09 21:35:38 -0500

    Create model Category

---
+++ models/category.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const CategorySchema = Schema({
+  name: {
+    type: String,
+    required: [true, "The name is required"],
+  },
+
+  isVisible: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = model("Category", CategorySchema);
