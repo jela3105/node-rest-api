3c965f974363d8505d3db60db9c3f4aedec4e08e
jela3105 <jela3105@gmail.com>
2021-05-15 23:47:50 -0500

    Upload file with post request

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,7 +1,22 @@
+const path = require("path");
 const { response } = require("express");
 
 const uploadFile = (req, res = response) => {
-  res.json({ msg: "hello word" });
+  if (!req.files || Object.keys(req.files).length === 0) {
+    res.status(400).json({ msg: "No files were uploaded." });
+    return;
+  }
+  const { file } = req.files;
+
+  uploadPath = path.join(__dirname, "../uploads/" + file.name);
+
+  file.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({ err });
+    }
+
+    res.json({ msg: "File uploaded to " + uploadPath });
+  });
 };
 
 module.exports = { uploadFile };
