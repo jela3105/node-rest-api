7ee9914ab58c77c8fca94129925c4bb97b16d8e4
jela3105 <jela3105@gmail.com>
2021-05-18 17:13:15 -0500

    Validate if user of product exists in update

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const { uploadFile } = require("../helpers/upload-file");
+const { User, Product } = require("../models");
 
 const postImage = async (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0) {
@@ -12,6 +13,29 @@ const postImage = async (req, res = response) => {
 
 const updateImage = async (req, res = response) => {
   const { id, collection } = req.params;
+  let model;
+
+  switch (collection) {
+    case "users":
+      model = await User.findById(id);
+      if (!model) {
+        return res
+          .status(400)
+          .json({ msg: `The user with the id ${id} not exists` });
+      }
+      break;
+    case "products":
+      model = await Product.findById(id);
+      if (!model) {
+        console.log("no se encontro");
+        return res
+          .status(400)
+          .json({ msg: `The product with the id ${id} not exists` });
+      }
+      break;
+    default:
+      return res.status(500).json({ msg: `collection not found` });
+  }
   res.json({ id, collection });
 };
 
