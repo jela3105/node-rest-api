91a74f07d9957b6f0dd295caa2e8fa2f695b6748
jela3105 <jela3105@gmail.com>
2021-05-20 00:03:06 -0500

    Get image working

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -52,8 +52,45 @@ const updateImage = async (req, res = response) => {
   res.json(model);
 };
 
-const getImage = (req, res = response) => {
-  res.json(req.params);
+const getImage = async (req, res = response) => {
+  const { id, collection } = req.params;
+  let model;
+
+  switch (collection) {
+    case "users":
+      model = await User.findById(id);
+      if (!model) {
+        return res
+          .status(400)
+          .json({ msg: `The user with the id ${id} not exists` });
+      }
+      break;
+    case "products":
+      model = await Product.findById(id);
+      if (!model) {
+        console.log("no se encontro");
+        return res
+          .status(400)
+          .json({ msg: `The product with the id ${id} not exists` });
+      }
+      break;
+    default:
+      return res.status(500).json({ msg: `collection not found` });
+  }
+
+  if (model.image) {
+    const imagePath = path.join(
+      __dirname,
+      "../uploads/",
+      collection,
+      model.image
+    );
+    if (fs.existsSync(imagePath)) {
+      return res.sendFile(imagePath);
+    }
+  }
+
+  res.json({ msg: "There is not placeholder yet" });
 };
 
 module.exports = { postImage, updateImage, getImage };
