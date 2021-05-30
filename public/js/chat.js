ce066c07948b40caa7e807bc81738e6eb6affa6b
jela3105 <jela3105@gmail.com>
2021-05-29 23:00:18 -0500

    Validate if JWT exists

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,16 @@
-const socket = io();
+//const socket = io();
+
+let user = null;
+let socket = null;
+
+const validateJWT = async () => {
+  const token = localStorage.getItem("token") || "";
+  if (token.length <= 10) {
+    window.location = "index.html";
+    throw new Error("No token found");
+  }
+};
+const main = async () => {
+  await validateJWT();
+};
+main();
