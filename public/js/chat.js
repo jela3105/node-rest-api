c93c7a65e7ae77a697e00b20615bcb39175216aa
jela3105 <jela3105@gmail.com>
2021-05-29 23:29:21 -0500

    Validate JWT in frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,9 @@
 //const socket = io();
 
+const url = window.location.hostname.includes("localhost")
+  ? "http://localhost:3000/api/auth/"
+  : "https://node-rest-api-jela.herokuapp.com/api/auth/";
+
 let user = null;
 let socket = null;
 
@@ -9,6 +13,12 @@ const validateJWT = async () => {
     window.location = "index.html";
     throw new Error("No token found");
   }
+  const resp = await fetch(url, {
+    headers: { "x-token": token },
+  });
+  const { user: userDB, token: tokenDB } = await resp.json();
+  localStorage.setItem("token", tokenDB);
+  user = userDB;
 };
 const main = async () => {
   await validateJWT();
